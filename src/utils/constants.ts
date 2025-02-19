8a8770dd61fe386f354cda5fb4ae310c2c73f9a3
MislanGigisly <homster100@yandex.ru>
2025-02-20 00:26:35 +0300

    add link for deploy

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,7 @@
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
+//Одинаковая часть пути для API_URL и CDN_URL также есть в переменной в файле .env
+//Но тут путь указан явно для прапвильной работоспособности deploy на github
+export const API_URL = 'https://larek-api.nomoreparties.co/api/weblarek';
+export const CDN_URL = 'https://larek-api.nomoreparties.co/content/weblarek';
 
 export const settings = {
 
